88ae14643e6c4d5c4be381d89b8b7fa0953865b0
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-14 16:47:11 +0000

    Add project files.

---
+++ InsuranceSevvice.Test/Test.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SQA_CA_1;
+
+namespace InsuranceSevvice.Test
+{
+    public class Test
+    {
+        [Test]
+        public void Test1()
+        {
+            //18 AND URBAN
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(18, "urban");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(50));
+        }
+        [Test]
+        public void Test2()
+        {
+            //31 AND URBAN
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(31, "urban");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(35));
+        }
+        [Test]
+        public void Test3()
+        {
+            //51 AND URBAN
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(51, "urban");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(30));
+        }
+        [Test]
+        public void Test4()
+        {
+            //18 AND RURAL
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(18, "rural");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(60));
+        }
+        [Test]
+        public void Test5()
+        {
+            //36 AND RURAL
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(36, "rural");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(50));
+        }
+        [Test]
+        public void Test6()
+        {
+            //51 AND RURAL
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(51, "rural");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(25));
+        }
+        [Test]
+        public void Test7()
+        {
+            //17 (UNDERAGE) AND RURAL
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(17, "urban");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test8()
+        {
+            //18 AND FDEGHWJHEEDHJEGR (INVALID LOCATION)
+            //arrange
+            InsuranceService p1 = new InsuranceService();
+            //act
+            double actualResult = p1.CalcPremium(18, "FDEGHWJHEEDHJEGR");
+            //assert
+            Assert.That(actualResult, Is.EqualTo(0));
+        }
+    }
+}
+
---
+++ SQA_CA_1/InsuranceService.cs
@@ -0,0 +1,72 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date : 9-3-2019
+ * Description : CA1 C# Code for Software Quality and Testing
+ *
+ * 
+ * ############################# */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQA_CA_1
+{
+    public class InsuranceService
+    {
+        public double CalcPremium(int age, string location)
+        {
+            double premium;
+
+            if (location == "urban")
+            {
+                if ((age >= 18) && (age <= 30))
+                    premium = 5.0;
+                else
+                {
+                    if (age >= 31)
+                    {
+                        premium = 2.50;
+                    }
+                    else
+                    {
+                        premium = 0.0;
+                    }
+                    premium = 0.0;
+                }
+            }
+            else
+            if (location == "rural")
+            {
+                if ((age >= 18) && (age <= 35))
+                {
+                    premium = 60;
+                }
+                else
+                {
+                    if (age >= 36)
+                    {
+                        premium = 50;
+                    }
+                    else
+                    {
+                        premium = 0.0;
+                    }
+                }
+            }
+            else
+            {
+                premium = 0.0;
+
+            }
+            if (age >= 50)
+            {
+                premium = premium * 0.15;
+
+            }
+            return premium;
+        }
+    }
+}
